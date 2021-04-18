572111da40a1b7fb076ce2d65f8a38aaf6d11be6
Nabeel <nabeelfarid@gmail.com>
2021-04-18 05:07:21 +0500

    Added Formik and Yup validation on Login Form

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -5,18 +5,21 @@ import {
   Card,
   CardContent,
   CardHeader,
+  CircularProgress,
   Grid,
   makeStyles,
-  TextField,
   Typography,
 } from "@material-ui/core";
 import { green } from "@material-ui/core/colors";
 import { LockOpen } from "@material-ui/icons";
-import { useRef } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import diariesApi from "../diariesApi";
 import { saveToken, setAuthState, setUser } from "../diariesSlice";
 import { useAppDispatch } from "../hooks";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikMuiTextField from "./FormikMuiTextField";
 
 interface LocationState {
   from: { pathname: string };
@@ -30,9 +33,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Login: React.FC = () => {
-  // let history = useHistory();
   let location = useLocation();
-  // let auth = useAuth();
 
   let { from } = (location.state || {
     from: { pathname: "/" },
@@ -41,31 +42,7 @@ const Login: React.FC = () => {
 
   const classes = useStyles();
   const navigate = useNavigate();
-  const txtUsername = useRef<HTMLInputElement>(null);
-  const txtPassword = useRef<HTMLInputElement>(null);
   const dispatch = useAppDispatch();
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const login = async () => {
-      try {
-        let username = txtUsername.current?.value as string;
-        let password = txtPassword.current?.value as string;
-        let user = await diariesApi.login(username, password);
-
-        dispatch(saveToken(user.token));
-        dispatch(setAuthState(true));
-        dispatch(setUser(user));
-        navigate(from.pathname, { replace: true });
-
-        console.log("login succesfull", user);
-      } catch (error) {
-        console.log("login error", error);
-      } finally {
-      }
-    };
-    login();
-  };
 
   return (
     <Box mx="auto" display="flex" justifyContent="center" maxWidth={400}>
@@ -79,50 +56,88 @@ const Login: React.FC = () => {
           }
         ></CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit} autoComplete="off">
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  inputRef={txtUsername}
-                  label="Username"
-                  variant="outlined"
-                  fullWidth
-                  defaultValue="tester"
-                  autoComplete="off"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  inputRef={txtPassword}
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                  fullWidth
-                  defaultValue="password"
-                  autoComplete="off"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  type="submit"
-                  fullWidth
-                >
-                  Sign in
-                </Button>
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  fullWidth
-                  style={{ textTransform: "none" }}
-                  onClick={() => navigate("../signup", { replace: true })}
-                >
-                  No Account? Create one
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            validationSchema={Yup.object({
+              username: Yup.string().required().min(6).max(20),
+              password: Yup.string().required().min(6).max(20),
+            })}
+            onSubmit={async (values, helpers) => {
+              // console.log(JSON.stringify(values, null, 2));
+              // await new Promise((r) => setTimeout(r, 3000));
+              // helpers.setSubmitting(false);
+
+              try {
+                let user = await diariesApi.login(
+                  values.username,
+                  values.password
+                );
+
+                dispatch(saveToken(user.token));
+                dispatch(setAuthState(true));
+                dispatch(setUser(user));
+
+                navigate(from.pathname, { replace: true });
+
+                console.log("login succesfull", user);
+              } catch (error) {
+                console.log("login error", error);
+                helpers.setSubmitting(false);
+              } finally {
+              }
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <FormikMuiTextField
+                      name="username"
+                      label="Username"
+                      variant="outlined"
+                      fullWidth
+                      autoComplete="off"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormikMuiTextField
+                      name="password"
+                      label="Password"
+                      variant="outlined"
+                      type="password"
+                      fullWidth
+                      autoComplete="off"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      startIcon={
+                        props.isSubmitting && <CircularProgress size="1rem" />
+                      }
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      fullWidth
+                      disabled={props.isSubmitting}
+                    >
+                      Sign in
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      fullWidth
+                      style={{ textTransform: "none" }}
+                      onClick={() => navigate("../signup", { replace: true })}
+                    >
+                      No Account? Create one
+                    </Button>
+                  </Grid>
+                </Grid>
+                <pre>{JSON.stringify(props.errors, null, 4)}</pre>
+                <pre>{JSON.stringify(props.values, null, 4)}</pre>
+              </Form>
+            )}
+          </Formik>
         </CardContent>
       </Card>
     </Box>
